ddce0eefff0624335a704ba9590969dbdb2b056b
askerlee <shaohua@gmail.com>
2021-12-23 14:32:36 +0800

    add droppath

--- core/setrans.py
+++ core/setrans.py
@@ -38,6 +38,35 @@ def gen_all_indices(shape, device):
         indices = torch.div(indices, dim_size, rounding_mode='trunc')
     return torch.stack(tuple(reversed(out)), len(shape))
 
+# drop_path and DropPath are copied from timm/models/layers/drop.py
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
 
 class SETransConfig(object):
     def __init__(self):
@@ -80,6 +109,7 @@ class SETransConfig(object):
         # Randomness settings
         self.hidden_dropout_prob = 0.1
         self.attention_probs_dropout_prob = 0.2
+        self.drop_path_prob = 0.1
         self.pos_code_type          = 'bias'
         self.ablate_multihead       = False
         self.out_attn_probs_only    = False
@@ -289,6 +319,7 @@ class ExpandedFeatTrans(nn.Module):
         self.base_initializer_range = config.base_initializer_range
         self.has_FFN        = getattr(config, 'has_FFN', True)
         self.has_input_skip = getattr(config, 'has_input_skip', False)
+        self.drop_path = DropPath(config.drop_path_prob) if config.drop_path_prob > 0. else nn.Identity()
 
         print0("{}: v_has_bias: {}, has_FFN: {}, has_input_skip: {}".format(
               self.name, config.v_has_bias, self.has_FFN, self.has_input_skip))
@@ -372,7 +403,7 @@ class ExpandedFeatTrans(nn.Module):
 
         # Have to ensure U1 == U2.
         if self.has_input_skip:
-            trans_feat = trans_feat + self.input_skip_coeff * input_feat
+            trans_feat = self.input_skip_coeff * input_feat + self.drop_path(trans_feat)
             trans_feat = self.skip_layer_norm(trans_feat)
             
         # trans_feat: [B, U1, 1792]
